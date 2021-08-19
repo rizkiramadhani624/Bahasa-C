2bf9b4badac8814739fd44ae0790ec8e37aed08f
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-08-19 16:50:29 +0700

    Luas Persegi

---
+++ Luas Persegi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+int Sisi,Luas;
+printf("Masukkan Panjang Sisi:");
+scanf("%d",&Sisi);
+
+Luas = Sisi*Sisi;
+printf("\nLuas Dari Persegi:%d", Luas);
+
+return(0);
+}
