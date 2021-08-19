9dd4296623795bf7b38f959d52ab114c4bc46d4a
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-08-19 18:21:40 +0700

    Add files via upload

---
+++ Rumus Luas Lingkaran.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+float radius, Luas;//
+
+printf ("\nMasukkan Nilai jari-jari Lingkaran = ");
+scanf("%f",&radius);
+
+Luas = 3.14*radius*radius;
+printf("\nNilai Luas Lingkaran Adalah =%5.2f", Luas);
+
+return (0);
+}
