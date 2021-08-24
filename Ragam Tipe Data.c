f270eecff4a486fded56b6d7b74a3f78676ab620
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-08-24 17:09:57 +0700

    Ragam Tipe Data int, double, char

---
+++ Ragam Tipe Data.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+NAMA    : Rizki Ramadhani
+NIM     : 160801087
+JUDUL   : Ragam Tipe Data
+*/
+
+int main(void)
+{
+  int      var1 = 98;
+  double   var2 = 0.314;
+  char     var3 = 'A';
+  int   var4[5] = {10, 20, 30, 40, 50};
+  char var5[11] = "All Is Well";
+
+  printf("%d \n",var1);
+  printf("%f \n",var2);
+  printf("%c \n",var3);
+  printf("%d \n",var4[3]);
+  printf("%s \n",var5);
+
+  printf("\n");
+  return 0;
+}
