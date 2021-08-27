8dc1af5620059b13a5e2d2f3f4e8df2cefaf8da8
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-08-27 22:32:21 +0700

    Add files via upload

---
+++ Tipe Data String.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+NAMA    : Rizki Ramadhani
+NIM     : 160801087
+JUDUL   : Tipe Data String
+*/
+
+int main(void)
+{
+  char foo[8] = {'b','e','l','a','j','a','r','\0'};
+
+  printf("Isi variabel foo[0] adalah %c \n", foo[0]);
+  printf("Isi variabel foo[5] adalah %c \n", foo[5]);
+  printf("Isi variabel foo[7] adalah %c \n", foo[6]);
+  printf("Isi variabel foo[8] adalah %c \n", foo[7]);
+
+  return 0;
+}
+
+
+
