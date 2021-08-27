0085ac481373eade3bfd438a7561a3dae35fc100
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-08-27 22:33:57 +0700

    Add files via upload

---
+++ Menghitung Jumlah karakter String.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+NAMA    : Rizki Ramadhani
+NIM     : 160801087
+JUDUL   : Menghitung Jumlah karakter Tipe Data String
+*/
+
+int main(void)
+{
+  char foo[] = "Belajar C di Duniailkom";
+
+  printf("Selamat datang di %s \n", foo);
+  printf("Panjang string adalah %d \n", sizeof(foo));
+  return 0;
+}
