7137e9e4bab33560c5dcf4361e64e992be8b8b24
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-08-27 22:08:20 +0700

    Add files via upload

---
+++ Tipe Array 1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+NAMA    : Rizki Ramadhani
+NIM     : 160801087
+JUDUL   : Tipe Array 1
+*/
+
+int main(void)
+{
+  int bilangan[5] = {6, 9, -8, 24, -99};
+
+  printf("Isi array bilangan pertama: %d \n",bilangan[0]);
+  printf("Isi array bilangan kedua: %d \n",bilangan[1]);
+  printf("Isi array bilangan ketika: %d \n",bilangan[2]);
+  printf("Isi array bilangan keempat: %d \n",bilangan[3]);
+  printf("Isi array bilangan kelima: %d \n",bilangan[4]);
+
+  return 0;
+}
